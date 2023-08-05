1be41bde7e09d8397d309b26259a9daa73da764c
currusu <natachagreco1@hotmail.com>
2023-08-04 21:43:13 -0300

    Update en la ruta de las imagenes en stock

--- data/stock.js
+++ data/stock.js
@@ -5,7 +5,7 @@ const productos = [
     descripcion: "Torta clásica con un toque diferente",
     precio: 8000,
     peso: "2kgs",
-    imagen: "../images/CheesecakeconChocolate.jpg",
+    imagen: "./images/CheesecakeconChocolate.jpg",
     cantidad:1
 
 },
@@ -16,7 +16,7 @@ nombre:"Cheesecake Tradicional",
 descripcion: "Torta clásica y fresca",
 precio: 7000,
 peso:"2kgs",
-imagen:"../images/CheesecakeTradicional.jpg",
+imagen:"./images/CheesecakeTradicional.jpg",
 cantidad:1
 
 
@@ -28,7 +28,7 @@ nombre:"Cuadrados de chocolate",
 descripcion:"Un clásico que no puede faltar a la hora del café",
 precio:500,
 peso:"200grs",
-imagen:"../images/CuadradosdeChocolate.jpg",
+imagen:"./images/CuadradosdeChocolate.jpg",
 cantidad:1
 
 },
@@ -39,7 +39,7 @@ nombre:"Cuadrados de naranja",
 descripcion:"Un clásico para la hora del té",
 precio:400,
 peso:"200grs",
-imagen:"../images/CuadradosdeNaranja.jpg",
+imagen:"./images/CuadradosdeNaranja.jpg",
 cantidad:1,
 
 },
@@ -51,7 +51,7 @@ cantidad:1,
     descripcion:"Clásica tarta de limón con el más rico merengue y relleno suave",
     precio:5000,
     peso:"2kgs",
-    imagen:"../images/LemonPie.jpg",
+    imagen:"./images/LemonPie.jpg",
     cantidad:1,
 
 },
@@ -62,7 +62,7 @@ nombre:"Macarons",
 descripcion:"Delicatesen de la pastelería francesa con sus exóticos colores y sabores",
 precio:300,
 peso:"50grs",
-imagen:"../images/Macarons.jpg",
+imagen:"./images/Macarons.jpg",
 cantidad:1
 
 },
@@ -73,7 +73,7 @@ nombre:"Pavlova",
 descripcion:"Riquisimo postre con crema fresca, frutos rojos y merengue crocante",
 precio:6000,
 peso:"2kgs",
-imagen:"../images/Pavlova.jpg",
+imagen:"./images/Pavlova.jpg",
 cantidad:1
 
 },
@@ -85,7 +85,7 @@ cantidad:1
     descripcion:"Clásica torta que no puede faltar en tu mesa dulce",
     precio:7000,
     peso:"2kgs",
-    imagen: "../images/TortadeChocolate.jpg",
+    imagen: "./images/TortadeChocolate.jpg",
     cantidad:1
 
 },
@@ -96,7 +96,7 @@ nombre:"TortadeFrutilla",
 descripcion:"Torta de frutilla decorada con butter cream",
 precio:7500,
 peso:"2kgs",
-imagen:"../images/TortadeFrutilla.jpg",
+imagen:"./images/TortadeFrutilla.jpg",
 cantidad:1
 
 },
@@ -108,7 +108,7 @@ cantidad:1
     descripcion:"Torta decorada con estas hermosas flores hechas en butter cream",
     precio:10000,
     peso:"2kgs",
-    imagen:"../images/TortaFloresRusas.jpg",
+    imagen:"./images/TortaFloresRusas.jpg",
     cantidad:1
 },
 
